6395ed49c93543d79f9a71c186c82b9d3242d43d
Laurens <laurensmeijerink@gmail.com>
2022-12-01 06:19:45 +0100

    day 01 done

--- Day01/day01.go
+++ Day01/day01.go
@@ -3,15 +3,53 @@ package main
 import (
 	"adventofcode2022/filereader"
 	"fmt"
+	"strconv"
 )
 
 func main() {
 	Assignment1()
+	Assignment2()
 }
 
 func Assignment1() {
-	strings := filereader.ReadFile("../Datafiles/day01.txt")
-	for _, s := range strings {
-		fmt.Println(s)
+	max := int64(0)
+	cur := int64(0)
+	for _, s := range filereader.ReadFile("../Datafiles/day01.txt") {
+		if s != "" {
+			num, _ := strconv.ParseInt(s, 0, 64)
+			cur += num
+		} else {
+			if cur > max {
+				max = cur
+			}
+			cur = int64(0)
+		}
 	}
+	fmt.Println(max)
+}
+
+func Assignment2() {
+	max1 := int64(0)
+	max2 := int64(0)
+	max3 := int64(0)
+	cur := int64(0)
+	for _, s := range filereader.ReadFile("../Datafiles/day01.txt") {
+		if s != "" {
+			num, _ := strconv.ParseInt(s, 0, 64)
+			cur += num
+		} else {
+			if cur > max1 {
+				max3 = max2
+				max2 = max1
+				max1 = cur
+			} else if cur > max2 {
+				max3 = max2
+				max2 = cur
+			} else if cur > max3 {
+				max3 = cur
+			}
+			cur = int64(0)
+		}
+	}
+	fmt.Println(max1 + max2 + max3)
 }
